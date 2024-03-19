a4d05512a2296d26a3c49621ac117626cd9d957a
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-19 18:33:15 +0200

    Add auth service

---
+++ src/app/auth/shared/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { STORAGE } from 'src/app/shared/constants/storage.constants';
+import { IUser } from 'src/app/shared/types/user-type';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  api = environment.api;
+  STORAGE = STORAGE;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(username: string, password: string): Observable<IUser> {
+    return this.http.post<IUser>(`${this.api}/login`, { username, password }).pipe(
+      map((user: IUser) => {
+        localStorage.setItem(this.STORAGE.USER, JSON.stringify(user));
+        return user;
+      })
+    )
+  }
+  logout() {
+    localStorage.removeItem(this.STORAGE.USER);
+  }
+  getUser(): IUser | null {
+    const user = localStorage.getItem(this.STORAGE.USER);
+    return user ? JSON.parse(user) : null;
+  }
+  isAuthenticated(): boolean {
+    return !!this.getUser();
+  }
+}
