3445768dc9cb0314ef730c30a54c6bf71ac5831f
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-20 11:58:57 +0200

    Add new types

---
+++ src/app/shared/types/appointment-type.ts
@@ -0,0 +1,13 @@
+export interface IAppointment {
+    id: number;
+    firstName: string;
+    lastName?: string;
+    address?: string;
+    phoneNumber: string;
+    appointmentDate: string;
+    created_at: string;
+    gender: string;
+    status: string;
+    updated_at: string;
+    dob: string;
+}
