6dc9b14401805690c63d0bbf05fe3adfc8031538
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-20 12:03:45 +0200

    Add status to color pipe

---
+++ src/app/shared/pipes/status-to-color.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'status_to_color'
+})
+export class StatusToColorPipe implements PipeTransform {
+
+  transform(status: string): string {
+    if (status === 'PENDING') {
+      return 'orange';
+    }
+    if (status === 'COMPLETED') {
+      return 'green';
+    }
+    if (status === 'CANCELED') {
+      return 'red';
+    }
+    return '';
+  }
+}
