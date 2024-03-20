848700a5cb1429956c97dce7162b0deb8f7aa4f1
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-20 12:03:13 +0200

    Update auth guard

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -2,26 +2,29 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {ROUTE} from "../constants/routes-constants";
+import { AuthService } from 'src/app/auth/shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  isAuth = false;
   ROUTE = ROUTE;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.isAuth) {
+      console.log('AuthGuard', this.authService.isAuthenticated());
+      
+    if (!this.authService.isAuthenticated()) {
       this.router.navigate([`/${this.ROUTE.AUTH}`]);
     }
-    return this.isAuth;
+    return true
   }
 
 
